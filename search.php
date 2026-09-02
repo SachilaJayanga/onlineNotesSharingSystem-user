2113b8e0eee46b08c0648e8c3a06c21ac53f18fb
Sachila Jayanga <171365598+SachilaJayanga@users.noreply.github.com>
2026-09-02 14:47:58 +0530

    Update search.php

--- search.php
+++ search.php
@@ -1,52 +1,31 @@
 <?php 
 session_start();
 include('partials-front/navbar.php'); 
-?>
-
-<!-- fOOD sEARCH Section Starts Here -->
-<!-- sEARCH Section Starts Here -->
+?> 
 <section class="search text-center">
     <div class="container">
-        <?php 
-
-            $search = mysqli_real_escape_string($conn, $_POST['search']);
-        
-        ?>
-
-
+        <?php  
+            $search = mysqli_real_escape_string($conn, $_POST['search']); 
+        ?> 
         <h2>Search Your Notes <a href="#" class="text-white">"<?php echo $search; ?>"</a></h2>
 
     </div>
-</section>
-<!-- sEARCH Section Ends Here -->
+</section>   
 
-
-
-<!-- MEnu Section Starts Here -->
 <section class="menu">
     <div class="container">
         <h2 class="text-center">Menu</h2>
 
-        <?php 
-
-            //SQL Query to Get Notess based on search keyword
-            
-            $sql = "SELECT * FROM tbl_notes WHERE title LIKE '%$search%'";
-
-            
-            $res = mysqli_query($conn, $sql);
-
-            
+        <?php  
+            $sql = "SELECT * FROM tbl_notes WHERE title LIKE '%$search%'"; 
+            $res = mysqli_query($conn, $sql); 
             $count = mysqli_num_rows($res);
 
-            //Check whether food available of not
-            if($count>0)
+             if($count>0)
             {
-                //Food Available
-                while($row=mysqli_fetch_assoc($res))
+                 while($row=mysqli_fetch_assoc($res))
                 {
-                    //Get the details
-                    $note_id = $row['note_id'];
+                     $note_id = $row['note_id'];
                     $title = $row['title'];                  
                     $description = $row['description'];
                     $image_name = $row['image_name'];
@@ -59,11 +38,9 @@ include('partials-front/navbar.php');
                         <div class=" ">
                        
                             <?php 
-                                //CHeck whether image available or not
-                                if($image_name=="")
+                                 if($image_name=="")
                                 {
-                                    //Image not Available 
-                                    ?>
+                                     ?>
                                      <img src="./images/default.png" alt="notes" class="img-responsive img-curve" width="60px">
                                     <?php    
                                 }
@@ -88,8 +65,7 @@ include('partials-front/navbar.php');
                             <button id="like-button-<?php echo $note_id; ?>" class="btn btn-like" onclick="toggleLike(<?php echo $note_id; ?>)">
 
                             <?php
-                            // Check if user already liked this note
-                            $account_id = $_SESSION['id'];
+                             $account_id = $_SESSION['id'];
                             $check_like_sql = "SELECT * FROM tbl_likes WHERE account_id = $account_id AND note_id = $note_id";
                             $check_like_res = mysqli_query($conn, $check_like_sql);
                             if (mysqli_num_rows($check_like_res) > 0) {
@@ -110,8 +86,7 @@ include('partials-front/navbar.php');
             }
             else
             {
-                //Notes not Available
-                echo "<div class='error'>Notes not found.</div>";
+                 echo "<div class='error'>Notes not found.</div>";
             }
         ?>
         <div class="clearfix"></div>
@@ -121,6 +96,5 @@ include('partials-front/navbar.php');
     </div>
     <script src="./js/like.js"></script>
 </section>
-<!-- Menu Section Ends Here -->
-
+ 
 <?php include('partials-front/footer.php'); ?>
