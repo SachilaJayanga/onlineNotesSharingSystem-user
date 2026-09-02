ab71da67474e4dc06ce5a6c3b8bfe08c7acd4df7
Sachila Jayanga <171365598+SachilaJayanga@users.noreply.github.com>
2026-09-02 14:36:23 +0530

    Update index.php

--- index.php
+++ index.php
@@ -5,19 +5,16 @@ include_once('partials-front/navbar.php');
 ?>
 <?php
 
-error_reporting(0);
-include('../config/constants.php');
-if (strlen($_SESSION['id']== 0)) {
-header('location:./login.php');
-} else{
-
-}
+    error_reporting(0);
+    include('../config/constants.php');
+    if (strlen($_SESSION['id']== 0)) {
+    header('location:./login.php');
+        }  
 
 ?>
 
 <?php
-// Get user ID from session
-
+ 
 $id = $_SESSION['id'];
 $sql = "SELECT * FROM tbl_accounts WHERE id = '$id'";
 $result = $conn->query($sql);
@@ -31,8 +28,7 @@ echo "No user found.";
 }
 ?>
 
-<!-- Profile Image and Greeting -->
-
+ 
 <div style="text-align: right; margin: 20px;">
     <img src="<?php echo $profile_image; ?>" alt="Profile Image " class="profile-image">
     <p style="align-content:center"><?php echo $username; ?></p>
@@ -43,10 +39,7 @@ echo "No user found.";
 <br><br>
 <h2 class="text-center"><font color=gray font-family=arial>Student Notes Sharing System</font></h2>
 <br><br>
-
-       
-        <!-- Main Content Setion Ends -->
-
+ 
         <section class="search text-center">
     <div class="container">
         
@@ -57,41 +50,31 @@ echo "No user found.";
         </form>
         
     </div>
-</section>
-<!-- search Section Ends Here -->
-
-
-<!-- Categories Section Starts Here -->
+</section> 
 <section class="categories">
     <div class="container">
         <h2 class="text-center">Explore Notes</h2>
 
         <?php 
-            // SQL Query to Display Categories from Database
-            $sql = "SELECT * FROM tbl_subjects WHERE active='Yes' LIMIT 6";
+             $sql = "SELECT * FROM tbl_subjects WHERE active='Yes' LIMIT 6";
             $res = mysqli_query($conn, $sql);
             $count = mysqli_num_rows($res);
 
             if($count > 0) {
-                // Categories Available
-                while($row = mysqli_fetch_assoc($res)) {
-                    // Get the Values like id, title, image_name
-                    $id = $row['id'];
+                 while($row = mysqli_fetch_assoc($res)) {
+                     $id = $row['id'];
                     $title = $row['title'];
                     $image_name = $row['image_name'];
                     ?>
                     
-                    <!-- Updated Link to Category-Notes Page -->
-                        
+                         
                         <div class="btn box-3 float-container">
                             <a href="category.php?subject_id=<?php echo $id; ?>">
                             <?php 
                                 if ($image_name == "") {
-                                    // Display Message if Image not Available
-                                    echo "<img src='./images/default.png' alt='notes' class='img-responsive img-curve' width='50px'>";
+                                     echo "<img src='./images/default.png' alt='notes' class='img-responsive img-curve' width='50px'>";
                                 } else {
-                                    // Image Available
-                                    ?>
+                                     ?>
                                     <img src="./images/category/<?php echo $image_name; ?>" alt="<?php echo $title; ?>" class="img-responsive img-curve">
                                     <?php
                                 }
@@ -104,40 +87,29 @@ echo "No user found.";
                     <?php
                 }
             } else {
-                // Categories not Available
-                echo "<div class='error'>Category not Added.</div>";
+                 echo "<div class='error'>Category not Added.</div>";
             }
         ?>
         <div class="clearfix"></div>
     </div>
 </section>
-<!-- Categories Section Ends Here -->
-
-<!-- Trending Section Starts Here -->
-<section class="menu">
+ 
+ <section class="menu">
     <div class="container">
         <h2 class="text-center">Trending</h2>
             <hr>
         <?php 
         
          
-        //SQL Query
-        $sql2 = "SELECT * FROM tbl_notes WHERE active='Yes' ORDER BY likes DESC LIMIT 4";
-
-        //Execute the Query
-        $res2 = mysqli_query($conn, $sql2);
-
-        //Count Rows
-        $count2 = mysqli_num_rows($res2);
-
-        //CHeck whether food available or not
+         $sql2 = "SELECT * FROM tbl_notes WHERE active='Yes' ORDER BY likes DESC LIMIT 4"; 
+        $res2 = mysqli_query($conn, $sql2); 
+        $count2 = mysqli_num_rows($res2); 
         if($count2>0)
         {
              
             while($row=mysqli_fetch_assoc($res2))
             {
-                //Get all the values
-                $note_id = $row['note_id']; 
+                 $note_id = $row['note_id']; 
                 $title = $row['title'];
                 $image_name = $row['image_name'];
                 ?>
@@ -146,16 +118,13 @@ echo "No user found.";
                     <div class="menu-img">
                     <a href="note_page.php?note_id=<?php echo $note_id; ?>" class="btn-primary">
                         <?php 
-                            //Check whether image available or not
-                            if($image_name=="")
+                             if($image_name=="")
                             {
-                                //Image not Available
-                                echo "<img src='./images/default.png' alt='notes' class='img-responsive img-curve' width='50px'>";
+                                 echo "<img src='./images/default.png' alt='notes' class='img-responsive img-curve' width='50px'>";
                             }
                             else
                             {
-                                //Image Available
-                                ?>
+                                 ?>
                                 <img src="./images/icon/<?php echo $image_name; ?>" alt="image" class="img-responsive img-curve">
                                 <?php
                             }
@@ -174,20 +143,13 @@ echo "No user found.";
         }
         else
         {
-            // Not Available 
-            echo "<div class='error'>Food not available.</div>";
+             echo "<div class='error'>Food not available.</div>";
         }
         
         ?>
-
-        
-
-
-
+ 
         <div class="clearfix"></div>
-
-        
-
+ 
     </div>
 <hr><br>
     <p class="text-center">
