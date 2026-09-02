24c59508217c99e149157423e1bb0a574288647e
Sachila Jayanga <171365598+SachilaJayanga@users.noreply.github.com>
2026-09-02 14:44:04 +0530

    Update manage-user.php

--- manage-user.php
+++ manage-user.php
@@ -1,8 +1,7 @@
 <?php include('partials/navbar.php'); ?>
 
 
-        <!-- Main Content Section Starts -->
-        <div class="main-content">
+         <div class="main-content">
             <div class="wrapper">
                 <h1>Manage Users</h1>
 
@@ -11,8 +10,8 @@
                 <?php 
                     if(isset($_SESSION['add']))
                     {
-                        echo $_SESSION['add']; //Displaying Session Message
-                        unset($_SESSION['add']); //REmoving Session Message
+                        echo $_SESSION['add'];  
+                        unset($_SESSION['add']);  
                     }
 
                     if(isset($_SESSION['delete']))
@@ -48,8 +47,7 @@
                 ?>
                 <br><br><br>
 
-                <!-- Button to Add User -->
-                <a href="add-user.php" class="btn-primary">Add New User</a>
+                 <a href="add-user.php" class="btn-primary">Add New User</a>
 
                 <br /><br /><br />
 
@@ -64,35 +62,20 @@
 
                     
                     <?php 
-                        //Query to Get all Admin
-                        $sql = "SELECT * FROM tbl_admin";
-                        //Execute the Query
-                        $res = mysqli_query($conn, $sql);
-
-                        //CHeck whether the Query is Executed of Not
-                        if($res==TRUE)
+                         $sql = "SELECT * FROM tbl_admin";
+                         $res = mysqli_query($conn, $sql); 
+                         if($res==TRUE)
                         {
-                            // Count Rows to CHeck whether we have data in database or not
-                            $count = mysqli_num_rows($res); // Function to get all the rows in database
-
-                            $sn=1; //Create a Variable and Assign the value
-
-                            //CHeck the num of rows
+                             $count = mysqli_num_rows($res);  
+                            $sn=1;   
                             if($count>0)
-                            {
-                                //WE HAve data in database
+                            { 
                                 while($rows=mysqli_fetch_assoc($res))
-                                {
-                                    //Using While loop to get all the data from database.
-                                    //And while loop will run as long as we have data in database
-
-                                    //Get individual DAta
+                                { 
                                     $id=$rows['id'];
                                     $full_name=$rows['full_name'];
                                     $email=$rows['email'];
-                                    $username=$rows['username'];
-
-                                    //Display the Values in our Table
+                                    $username=$rows['username']; 
                                     ?>
                                     
                                     <tr>
@@ -113,7 +96,7 @@
                             }
                             else
                             {
-                                //We Do not Have Data in Database
+                                 echo "Do not Have Data in Database";
                             }
                         }
 
@@ -125,6 +108,5 @@
 
             </div>
         </div>
-        <!-- Main Content Setion Ends -->
-
+ 
 <?php include('partials/footer.php'); ?>
