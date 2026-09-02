67844e003e95ff713665df228569ce71bcd2a7d2
Sachila Jayanga <171365598+SachilaJayanga@users.noreply.github.com>
2026-09-02 14:27:31 +0530

    Update change_password.php

--- change_password.php
+++ change_password.php
@@ -4,19 +4,16 @@ include('./config/constants.php');
 
 if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['change_password'])) {
     $id = $_POST['id'];
-    $current_password = md5($_POST['current_password']); // Encrypt to match database format
+    $current_password = md5($_POST['current_password']); 
     $new_password = md5($_POST['new_password']);
     $confirm_password = md5($_POST['confirm_password']);
 
-    // Verify the current password
-    $sql = "SELECT * FROM tbl_accounts WHERE id = '$id' AND password = '$current_password'";
+     $sql = "SELECT * FROM tbl_accounts WHERE id = '$id' AND password = '$current_password'";
     $res = mysqli_query($conn, $sql);
 
     if (mysqli_num_rows($res) === 1) {
-        // Check if new password and confirm password match
-        if ($new_password === $confirm_password) {
-            // Update the password
-            $update_sql = "UPDATE tbl_accounts SET password = '$new_password' WHERE id = '$id'";
+         if ($new_password === $confirm_password) {
+             $update_sql = "UPDATE tbl_accounts SET password = '$new_password' WHERE id = '$id'";
             $update_res = mysqli_query($conn, $update_sql);
 
             if ($update_res) {
@@ -31,8 +28,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['change_password'])) {
         $_SESSION['user-not-found'] = "<div class='error'>Current password is incorrect.</div>";
     }
 
-    // Redirect back to the profile page
-    header('location:myprofile.php');
+     header('location:myprofile.php');
     exit();
 }
 ?>
